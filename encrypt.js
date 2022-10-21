6ac401fb0c485885d17307b1676cbe92b7986740
HarshithaSampath <110219821+HarshithaSampath@users.noreply.github.com>
2022-10-21 11:36:18 +0530

    Add files via upload

---
+++ encrypt.js
@@ -0,0 +1,21 @@
+var crypto = require('crypto');
+
+const algorithm = 'aes-192-cbc';
+const password = 'dharaniiii';
+
+crypto.scrypt(password, 'salt', 24, (err, key) => {
+  if (err) throw err;
+  crypto.randomFill(new Uint8Array(16), (err, iv) => {
+    if (err) throw err;
+
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+    let encrypted = '';
+    cipher.setEncoding('hex');
+
+    cipher.on('data', (chunk) => encrypted += chunk);
+    cipher.on('end', () => console.log(encrypted));
+    cipher.write('some clear text data');
+    cipher.end();
+  });
+});
